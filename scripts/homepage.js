cbedd339ff7b3bd049ff7d4a190d852ea9fb3c95
Elisabetta Piacquadio <betta.pcq@gmail.com>
2025-11-13 00:45:38 +0100

    add album to hompage

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -101,15 +101,132 @@ const getSongDetails = () => {
 
 getSongDetails();
 
-const albumsArray = [
-  '1127912',
-  '851331162',
-  '324179237',
-  '8446705',
-  '104188',
-  '72704062'
-];
+// funzione riempi albums
+const albumsArray1 = ['1127912', '851331162', '324179237', '8446705', '104188'];
 
+const albumsArray2 = ['13433823', '552945182', '8446705'];
+
+const loadAlbumsSmallCards = function () {
+  for (let i = 0; i < albumsArray1.length; i++) {
+    const albumID = albumsArray1[i];
+    fetch(albumUrl + albumID)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error('Ops! Errore dalla risposta: ' + res.status);
+        }
+      })
+      .then((albumData) => {
+        const artistAlbums = document.getElementById('small-cards');
+        const cover = albumData.cover_medium;
+        const title = albumData.title;
+
+        artistAlbums.innerHTML += `
+              <div class="card mb-3 border-0">
+              <div class="row g-0">
+                <div class="col col-4">
+                  <img
+                    src="${cover}"
+                    class="img-fluid rounded-start-1"
+                    alt="preview"
+                    style="min-width: 40px; min-height: 40px"
+                  />
+                </div>
+                <div class="col col-8">
+                  <div class="card-body">
+                    <h6 class="card-title">${title}</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+        `;
+      })
+      .catch((err) => {
+        console.log('error:', err);
+      });
+  }
+};
+
+loadAlbumsSmallCards();
+
+const loadAlbumsBigCards = function () {
+  for (let i = 0; i < albumsArray2.length; i++) {
+    const currentAlbumID = albumsArray2[i];
+    fetch(albumUrl + currentAlbumID)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error('Ops! Errore dalla risposta: ' + res.status);
+        }
+      })
+      .then((albumData) => {
+        console.log('album data', albumData);
+        const artistAlbums = document.getElementById('big-cards');
+        const cover = albumData.cover_medium;
+        const title = albumData.title;
+        const artist = albumData.artist.name;
+        const nTracks = albumData.nb_tracks;
+
+        artistAlbums.innerHTML += `
+           <div class="card bg-gradient border-0 rounded-3 mb-5">
+                    <div class="row m-3">
+                      <div class="col">
+                        <img
+                          src="${cover}"
+                          class="img-fluid shadow-lg"
+                          alt="preview"
+                          style="min-width: 45px; min-height: 45px"
+                        />
+                      </div>
+                      <div class="col">
+                        <div class="card-body">
+                           <a class="artistAncor" href="./artist-page.html"><p class="small text-secondary my-0">${artist}</p></a>
+                          <a class="albumAncor" href="./album-details.html"><h6 class="card-title my-0">${title}</h6></a>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col">
+                        <a class="link-success">
+                          <i class="bi bi-heart-fill"></i
+                        ></a>
+                        <i class="bi bi-three-dots-vertical"></i>
+                      </div>
+                      <div
+                        class="col d-flex align-items-center justify-content-end"
+                      >
+                        <p class="small text-secondary me-2 my-0">${nTracks} brani</p>
+
+                        <i
+                          class="btn btn-dark rounded-circle bi bi-play-fill fs-5 px-1 py-0"
+                        ></i>
+                      </div>
+                    </div>
+                  </div>
+        `;
+        const artistAncor = artistAlbums.querySelector('.artistAncor');
+        const artistID = albumData.artist.id;
+        const albumAncor = artistAlbums.querySelector('.albumAncor');
+        artistAncor.addEventListener('click', () => {
+          const artistURL = `https://striveschool-api.herokuapp.com/api/deezer/artist/${artistID}`;
+          localStorage.setItem('artistURL', artistURL);
+        });
+        albumAncor.addEventListener('click', () => {
+          const albumURL = `https://striveschool-api.herokuapp.com/api/deezer/album/${currentAlbumID}`;
+          localStorage.setItem('albumURL', albumURL);
+        });
+      })
+      .catch((err) => {
+        console.log('error:', err);
+      });
+  }
+};
+
+loadAlbumsBigCards();
+
+// funzione riempi amici:
 const artistArray = ['acdc', 'deeppurple', 'tupac', '50cent'];
 
 const fillFriendsAside = function () {
