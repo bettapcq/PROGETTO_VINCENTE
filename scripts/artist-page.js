554e1bbf087c7b38ca3583253327e72ddeb42920
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2025-11-14 16:47:55 +0100

    main fixing

--- scripts/artist-page.js
+++ scripts/artist-page.js
@@ -2,77 +2,66 @@ let artistURL = localStorage.getItem('artistURL');
 const searchURL = `https://striveschool-api.herokuapp.com/api/deezer/search?q=`;
 console.log(artistURL);
 
-// recuperare il contenuto del player
-const songTitle = localStorage.getItem('songTitle');
-const songAuthor = localStorage.getItem('songAuthor');
-const songCover = localStorage.getItem('songCover');
-const trackAudio = localStorage.getItem('trackAudio');
-const isPaused = localStorage.getItem('isPaused');
-console.log(isPaused);
-const boolean = isPaused === 'true';
+// player function
+const songTitle = localStorage.getItem("songTitle")
+const songAuthor = localStorage.getItem("songAuthor")
+const songCover = localStorage.getItem("songCover")
+const trackAudio = localStorage.getItem("trackAudio")
+const isPaused = localStorage.getItem("isPaused")
+console.log(isPaused)
+const boolean = isPaused === "true"
 
-const savedAudio = new Audio(trackAudio);
+const savedAudio = new Audio(trackAudio)
 
-let initialPaused;
-
-if (isPaused === null) {
-  initialPaused = true; // se non c’è nulla in localStorage, parte in pausa
-} else if (isPaused === 'true') {
-  initialPaused = true; // se è true inizia con pausa
-} else {
-  initialPaused = false; // se è false inizia con play
-}
-
-const fillPlayer = function (title, author, cover, audio) {
-  const playerText = document.getElementById('playerText');
-  const albumCoverPlayer = document.getElementById('album_small_cover');
-  const play_btns = document.getElementsByClassName('play_btns');
-  albumCoverPlayer.setAttribute('src', cover);
-  if (!initialPaused) {
-    savedAudio.play();
-  } else {
-    savedAudio.pause();
-  }
-
+const fillPlayer = function (title, author, cover, audio, pause) {
+  const playerText = document.getElementById("playerText")
+  const albumCoverPlayer = document.getElementById("album_small_cover")
+  const play_btns = document.getElementsByClassName("play_btns")
+  albumCoverPlayer.setAttribute("src", cover)
   playerText.innerHTML = `
                 <h6>${title}</h6>
                 <p>di ${author}</p>
                 <i
                     class="cuore bi bi-heart d-none d-md-inline-block position-absolute"
-                  ></i>`;
+                  ></i>`
+
+  // stato iniziale del player da local storage
+  if (pause === true) {
+    audio.pause()
+    for (let i = 0; i < play_btns.length; i++) {
+      play_btns[i].innerHTML =
+        '<i class="bi bi-play-fill play_btns fs-5 m-0"></i>'
+    }
+  } else {
+    audio.play()
+    for (let i = 0; i < play_btns.length; i++) {
+      play_btns[i].innerHTML = '<i class="bi bi-pause-fill fs-5 m-0"></i>'
+    }
+  }
 
   for (let i = 0; i < play_btns.length; i++) {
     play_btns[i].addEventListener(
-      'click',
+      "click",
       (togglePlay = () => {
         if (audio.paused) {
-          audio.pause();
-          localStorage.setItem('isPaused', true);
-
-          for (let i = 0; i < play_btns.length; i++) {
-            play_btns[
-              i
-            ].innerHTML = `<i class="bi bi-play-fill play_btns fs-5 m-0"></i>`;
+          audio.play()
+          localStorage.setItem("isPaused", "false")
+          for (let j = 0; j < play_btns.length; j++) {
+            play_btns[j].innerHTML = '<i class="bi bi-pause-fill fs-5 m-0"></i>'
           }
         } else {
-          audio.play();
-          for (let i = 0; i < play_btns.length; i++) {
-            play_btns[
-              i
-            ].innerHTML = `<i class="bi bi-pause-fill fs-5 m-0"></i>`;
+          audio.pause()
+          localStorage.setItem("isPaused", "true")
+          for (let j = 0; j < play_btns.length; j++) {
+            play_btns[j].innerHTML = '<i class="bi bi-play-fill fs-5 m-0"></i>'
           }
-          localStorage.setItem('isPaused', false);
         }
       })
-    );
+    )
   }
+}
 
-  for (let i = 0; i < play_btns.length; i++) {
-    play_btns[i].innerHTML = `<i class="bi bi-pause-fill fs-5 m-0"></i>`;
-  }
-};
-
-fillPlayer(songTitle, songAuthor, songCover, savedAudio);
+fillPlayer(songTitle, songAuthor, songCover, savedAudio, boolean)
 
 // riempire sezione header dell'artista:
 
